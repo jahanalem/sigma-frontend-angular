dabcb9a053005794bc698388044c1bcc95908a47
Said Roohullah Allem <s.r.alem19@gmail.com>
2022-08-26 15:28:45 +0200

    identity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AccountService } from './account/account.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
@@ -12,9 +13,25 @@ import { IProduct } from './shared/models/product';
 export class AppComponent implements OnInit {
   title = 'Sigma';
 
-  constructor(private basketService: BasketService) { }
+  constructor(private basketService: BasketService, private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem("token");
+
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log("loaded user");
+      }, error => {
+        console.log(error);
+      })
+
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
